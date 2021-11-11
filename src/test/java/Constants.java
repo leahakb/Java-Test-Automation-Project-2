d8366aae41837e01b9584d8d3b79f748bac43d88
Leah <leah@uxwebtech.com>
2021-11-11 16:17:57 +0200

    Java Test Automation Project 2

---
+++ src/test/java/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final String email = "leah.akbashev@gmail.com";
+    public static final String pass = "Test@543!";
+    public static final String email_id = "ember1469";
+    public static final String pass_id = "ember1472";
+    public static final String entrance_button_id = "ember1489";
+}
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,14 +1,25 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Main {
-    public static void main(String[] args) {
+
+
+
+    @BeforeClass
+    public static void openSiteAndLogin(){
         SingletonBrowser browser1 = SingletonBrowser.getInstaceOfSingletonBrowser();
         WebDriver driver = browser1.getDriver();
         driver.get("https://buyme.co.il/");
+        //login
+        driver.findElement(By.linkText("Login"));
+        driver.findElement(By.id(Constants.email_id)).sendKeys(Constants.email);
+        driver.findElement(By.id(Constants.pass_id)).sendKeys(Constants.pass);
+        driver.findElement(By.id(Constants.entrance_button_id)).click();
     }
     @Test
     public static void test_1()
@@ -20,4 +31,5 @@ public class Main {
     public static void runAfterClass(){
         //driver.quit();
     }
+
 }
