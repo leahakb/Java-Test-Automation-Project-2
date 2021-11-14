a80f1b72f24b8330e1d3894217a638cdd630b0b4
Leah <leah@uxwebtech.com>
2021-11-14 03:13:05 +0200

    Java Test Automation Project 2

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -1,7 +1,8 @@
 public class Constants {
-    public static final String email = "leah.akbashev@gmail.com";
+    //registration credentials
+    public static final String firstName = "test";
+    
+    //registration and login credentials
+    public static final String email = "leah@gmail.com";
     public static final String pass = "Test@543!";
-    public static final String email_id = "ember1469";
-    public static final String pass_id = "ember1472";
-    public static final String entrance_button_id = "ember1489";
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,34 +1,86 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class Main {
-
-
+    private static WebDriverWait wait;
+    private static WebDriver driver;
+    private boolean alreadyRegistered = true;
+    private boolean notLoggedIn = true;
 
     @BeforeClass
     public static void openSiteAndLogin(){
         SingletonBrowser browser1 = SingletonBrowser.getInstaceOfSingletonBrowser();
-        WebDriver driver = browser1.getDriver();
+        driver = browser1.getDriver();
         driver.get("https://buyme.co.il/");
-        //login
-        driver.findElement(By.linkText("Login"));
-        driver.findElement(By.id(Constants.email_id)).sendKeys(Constants.email);
-        driver.findElement(By.id(Constants.pass_id)).sendKeys(Constants.pass);
-        driver.findElement(By.id(Constants.entrance_button_id)).click();
+        wait = new WebDriverWait(driver, 10);
     }
     @Test
-    public static void test_1()
-    {
+    private void toRegister() {
+        //popup to login or register
+        if (!alreadyRegistered) {
+            driver.findElement(By.className("seperator-link")).click();
+            //wait for the "to register" link element to be availabe. Popup needs time to get loaded
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.register-or-login > span")));
 
+            driver.findElement(By.cssSelector("div.register-or-login > span")).click();
+
+            driver.findElement(By.cssSelector("input[placeholder='שם פרטי']")).sendKeys(Constants.firstName);
+            driver.findElement(By.cssSelector("input[placeholder='מייל']")).sendKeys(Constants.email);
+            driver.findElement(By.id("valPass")).sendKeys(Constants.pass);
+            driver.findElement(By.cssSelector("input[placeholder='אימות סיסמה']")).sendKeys(Constants.pass);
+            driver.findElement(By.cssSelector("button[type=submit]")).click();
+
+            alreadyRegistered = driver.findElement(By.className("login-error")).isDisplayed();
+            if (alreadyRegistered) {
+                driver.findElement(By.className("lightbox-close")).click();
+
+            }
+        }
     }
 
+    @Test
+    public void toLogin(){
+        notLoggedIn = driver.findElement(By.className("seperator-link")).isDisplayed();
+        if(!notLoggedIn)
+            System.out.println("You are already logged in");
+        else{
+            //popup to login or register
+            driver.findElement(By.className("seperator-link")).click();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[gtm='כניסה ל-BUYME']")));
+            WebElement login = driver.findElement(By.cssSelector("button[type=submit]"));
+            Actions login_aciton = new Actions(driver);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type=email]")));
+            driver.findElement(By.cssSelector("input[type=email]")).sendKeys(Constants.email);
+            driver.findElement(By.cssSelector("input[type=password]")).sendKeys(Constants.pass);
+
+            login_aciton.moveToElement(login).click().perform();
+        }
+    }
+    @Test
+    public void searchGift(){
+
+    }
+    @Test
+    public void pickBusiness(){
+
+    }
+    @Test
+    public void sendRecieveInfo(){
+
+    }
     @AfterClass
-    public static void runAfterClass(){
+    public void runAfterClass(){
         //driver.quit();
     }
 
